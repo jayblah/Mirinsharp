9db4635d6ff92001f45279618486a1bc49381827
mirinsharp <mirinsharp@outlook.com>
2015-09-22 20:49:25 +0200

    fix

--- Shy Riven/Program.cs
+++ Shy Riven/Program.cs
@@ -22,6 +22,9 @@ namespace ShyRiven
 
         static void Game_OnGameLoad(EventArgs args)
         {
+            if (ObjectManager.Player.CharData.BaseSkinName != "Riven")
+                return;
+
             Champion = new Riven();
 
             Champion.CreateConfigMenu();
